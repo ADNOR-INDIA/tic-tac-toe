4c346fabb6b4a7a95bef0e5cab3ab13a8c2ba2fa
Abhay <adnorindia@gmail.com>
2023-11-22 09:46:22 +0530

    Lifted up the state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-const Square=()=>{
+import { useState } from "react";
+import Square from "./components/Square";
+
+const Board=()=>{
+  const [square, setSquare] = useState(Array(9).fill(null));
   return(
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="swaure">X</button>
-        <button className="swaure">X</button>
+        <Square value={square[0]}/>
+        <Square value={square[1]}/>
+        <Square value={square[2]}/>
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="swaure">X</button>
-        <button className="swaure">X</button>
+        <Square value={square[3]}/>
+        <Square value={square[4]}/>
+        <Square value={square[5]}/>
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="swaure">X</button>
-        <button className="swaure">X</button>
+        <Square value={square[6]}/>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
       </div>
     </>
     
   );
 }
 
-export default Square
+export default Board
